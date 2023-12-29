45242e4c67c89b17bea87ad45159a8cb630405d5
Finley Ge <m13203533462@163.com>
2023-12-29 19:42:51 +0800

    fix(init.sh): fix the init.sh

--- init.sh
+++ init.sh
@@ -8,31 +8,12 @@ if [ $project_name_correct = "n" ]; then
     echo "Enter project name:"
     read project_name
 fi
-echo "If you want to connect to a remote repository? (y/n)"
-read remote_repo
-if [ $remote_repo = "y" ]; then
-    echo "Enter remote repository url:"
-    read remote_repo_url
-    echo "Enter remote repository branch:"
-    read remote_repo_branch
-fi
 rm ./README.md
 rm ./init.sh
-
-echo "# $project_name" >> README.md
 rm -r ./.git
-
+find . -type f -exec sed -i 's/Go-Server-Scaffold/test/g' {} +
+echo "# $project_name" >> README.md
+rm ./go.mod
+rm ./go.sum
+go mod init $project_name
 git init
-git remote add origin $remote_repo_url
-git pull origin $remote_repo_branch
-git add .
-git commit -m "Initial commit"
-if [ $remote_repo = "y" ]; then
-    git push -u origin $remote_repo_branch
-fi
-
-echo "Project initialized!"
-echo "Your project name is $project_name"
-echo "Your remote repository is $remote_repo_url"
-echo "Your remote repository branch is $remote_repo_branch"
-echo "Thank you for using this scaffold!"
